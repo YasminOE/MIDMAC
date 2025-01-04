173b11f19368011161e05c614def7f6a3321a51a
Yassmin Elmadhi <elmahdiyasmin@gmail.com>
2025-01-04 22:44:41 +0300

    Fix contacts section

--- midmac-website/src/components/ui/home/ContactsComponent.tsx
+++ midmac-website/src/components/ui/home/ContactsComponent.tsx
@@ -1,76 +1,3 @@
-// import React from 'react'
-// import { ContactsBlock as ContactsBlockProps } from '@/payload-types'
-
-// // TODO: Fix the contact info section on both mobile and desktop
-
-// type Props = {
-//   className?: string
-// } & ContactsBlockProps 
-
-// export const Contacts: React.FC<Props> = ({ 
-//   contactInfo, 
-//   rightContent 
-// }) => {
-//     const ContactInfo = JSON.parse(JSON.stringify(contactInfo))
-//     const RightContent = JSON.parse(JSON.stringify(rightContent))
-//   return (
-//     <section 
-//       id="contact" 
-//       className="h-[50vh] container large"
-//     >
-//       <div className="w-full h-full max-w-[2690px] mx-auto px-[--container-padding] flex flex-col-reverse md:flex-row justify-start md:justify-between items-center gap-6 md:gap-12">
-
-//         {/* Left side - Contact Information */}
-//         <div className="w-full md:w-1/2 3xl:max-w-[800px]">
-//           <div className="flex flex-col justify-between items-start md:items-center border-b border-[#DAD2C2] border-t border-[#DAD2C2] pb-2 md:pb-1 pt-2 md:pt-1 gap-2 md:flex-row">
-//             <span className="text-sm 3xl:text-2xl uppercase">
-//               {ContactInfo.emailLabel}
-//             </span>
-//             <span className="text-[#DAD2C2] text-sm 3xl:text-xl">
-//               {ContactInfo.email}
-//             </span>
-//           </div>
-          
-//           <div className="flex flex-col justify-between items-start md:items-center border-b border-[#DAD2C2] pb-2 md:pb-1 mt-4 md:mt-2 gap-2 md:flex-row">
-//             <span className="text-sm 3xl:text-2xl uppercase">
-//               {ContactInfo.instagramLabel}
-//             </span>
-//             <span className="text-[#DAD2C2] text-sm 3xl:text-xl">
-//               {ContactInfo.instagram}
-//             </span>
-//           </div>
-          
-//           <div className="flex flex-col justify-between items-start md:items-center border-b border-[#DAD2C2] pb-2 md:pb-1 mt-4 md:mt-2 gap-2 md:flex-row">
-//             <span className="text-sm 3xl:text-2xl uppercase">
-//               {ContactInfo.phoneLabel}
-//             </span>
-//             <span className="text-[#DAD2C2] text-sm 3xl:text-2xl">
-//               {ContactInfo.phone}
-//             </span>
-//           </div>
-//         </div>
-
-//         {/* Right side - Dynamic Content */}
-//         <div className="w-full md:w-1/2 flex justify-center md:justify-end mb-6 md:mb-0">
-//           {RightContent.type === 'button' ? (
-//             <a 
-//               href={RightContent.buttonLink || '/'}
-//               className="bg-[#E5E0D9] text-[#1E1E1E] px-12 md:px-24 py-4 md:py-8 rounded-lg text-xl md:text-2xl 3xl:text-3xl uppercase inline-block hovered-btn w-full md:w-auto text-center"
-//             >
-//               {RightContent.content}
-//             </a>
-//           ) : (
-//             <h2 className="text-[#E5E0D9] text-2xl 3xl:text-3xl">
-//               {RightContent.content}
-//             </h2>
-//           )}
-//         </div>
-//       </div>
-//     </section>
-//   )
-// }
-
-
 'use client'
 
 import React from 'react'
@@ -97,31 +24,44 @@ export const Contacts: React.FC<Props> = ({
 
         {/* Left side - Contact Information */}
         <div className="w-full md:w-1/2 3xl:max-w-[800px]">
-          <div className="flex flex-col justify-between items-start md:items-center border-b border-[#DAD2C2] border-t border-[#DAD2C2] pb-2 md:pb-1 pt-2 md:pt-1 gap-2 md:flex-row">
+          <div className="flex justify-between items-start md:items-center border-[#DAD2C2] border-b border-t pb-2 md:pb-1 pt-2 md:pt-1 gap-2 md:flex-row">
             <span className="text-sm 3xl:text-2xl uppercase">
               {ContactInfo.emailLabel}
             </span>
-            <span className="text-[#DAD2C2] text-sm 3xl:text-xl">
+            <a 
+              href={`mailto:${ContactInfo.email}`}
+              className="text-[#DAD2C2] text-sm 3xl:text-xl  transition-colors"
+            >
               {ContactInfo.email}
-            </span>
+            </a>
           </div>
           
-          <div className="flex flex-col justify-between items-start md:items-center border-b border-[#DAD2C2] pb-2 md:pb-1 mt-4 md:mt-2 gap-2 md:flex-row">
+          <div className="flex justify-between items-start md:items-center border-b border-[#DAD2C2] pb-2 md:pb-1 mt-4 md:mt-2 gap-2 md:flex-row">
             <span className="text-sm 3xl:text-2xl uppercase">
               {ContactInfo.instagramLabel}
             </span>
-            <span className="text-[#DAD2C2] text-sm 3xl:text-xl">
+            <a 
+              href="https://www.instagram.com/midmac.design/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-[#DAD2C2] text-sm 3xl:text-xl transition-colors"
+            >
               {ContactInfo.instagram}
-            </span>
+            </a>
           </div>
           
-          <div className="flex flex-col justify-between items-start md:items-center border-b border-[#DAD2C2] pb-2 md:pb-1 mt-4 md:mt-2 gap-2 md:flex-row">
+          <div className="flex justify-between items-start md:items-center border-b border-[#DAD2C2] pb-2 md:pb-1 mt-4 md:mt-2 gap-2 md:flex-row">
             <span className="text-sm 3xl:text-2xl uppercase">
               {ContactInfo.phoneLabel}
             </span>
-            <span className="text-[#DAD2C2] text-sm 3xl:text-2xl">
+            <a 
+              href={`https://wa.me/${ContactInfo.phone.replace(/[^0-9]/g, '')}`}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-[#DAD2C2] text-sm 3xl:text-xl transition-colors"
+            >
               {ContactInfo.phone}
-            </span>
+            </a>
           </div>
         </div>
 
--- midmac-website/src/components/ui/projects/Contacts.tsx
+++ midmac-website/src/components/ui/projects/Contacts.tsx
@@ -1,26 +1,45 @@
 export const Contact = () => {
   return (
     <section className="py-32">
-      <div className="grid grid-cols-2 gap-16">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-16">
         {/* Contact Information */}
         <div className="space-y-2">
-          <div className="flex justify-between border-t border-[#DAD2C2] py-1">
-            <span className="uppercase text-[1.1rem] tracking-wider">EMAIL</span>
-            <span className="text-[1.1rem]">info@midmac.design</span>
+          <div className="flex justify-between md:flex-row items-start md:items-center border-t border-[#DAD2C2] py-1 gap-2">
+            <span className="uppercase text-sm md:text-[1.1rem] tracking-wider">EMAIL</span>
+            <a 
+              href="mailto:info@midmac.design"
+              className="text-sm md:text-[1.1rem] transition-colors"
+            >
+              info@midmac.design
+            </a>
           </div>    
-          <div className="flex justify-between border-t border-[#DAD2C2] py-1">
-            <span className="uppercase text-[1.1rem] tracking-wider">INSTAGRAM</span>
-            <span className="text-[1.1rem]">midmac.design</span>
+          <div className="flex justify-between md:flex-row items-start md:items-center border-t border-[#DAD2C2] py-1 gap-2">
+            <span className="uppercase text-sm md:text-[1.1rem] tracking-wider">INSTAGRAM</span>
+            <a 
+              href="https://www.instagram.com/midmac.design/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-sm md:text-[1.1rem] transition-colors"
+            >
+              midmac.design
+            </a>
           </div>
-          <div className="flex justify-between border-t border-[#DAD2C2] border-b py-1">
-            <span className="uppercase text-[1.1rem] tracking-wider">PHONE</span>
-            <span className="text-[1.1rem]">+966 56 322 2396</span>
+          <div className="flex justify-between md:flex-row items-start md:items-center border-t border-[#DAD2C2] border-b py-1 gap-2">
+            <span className="uppercase text-sm md:text-[1.1rem] tracking-wider">PHONE</span>
+            <a 
+              href="https://wa.me/966563222396"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-sm md:text-[1.1rem] transition-colors"
+            >
+              +966 56 322 2396
+            </a>
           </div>
         </div>
 
-        {/* Contact Us Title */}
-        <div className="flex items-center justify-end">
-          <h2 className="text-[4rem] font-light uppercase">CONTACT US</h2>
+        {/* Right side - Dynamic Content */}
+        <div className="flex items-center justify-start md:justify-end order-first md:order-last md:mb-0">
+          <h2 className="text-[2rem] md:text-[4rem] font-light uppercase">CONTACT US</h2>
         </div>
       </div>
     </section>
